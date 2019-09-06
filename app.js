11c8138c0d40c873df4de1ff22c3e1560dee5b53
Mark Leo <ratrateroo@gmail.com>
2019-09-06 23:59:50 +0800

    routed request

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const http = require('http');
 //nodejs use require for imports
 
 function rqListener(req, res){
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
+
+    const url = req.url;
+    if (url ==='/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form<</body');
+        res.write('<html>');
+        return res.end();
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -10,7 +19,7 @@ function rqListener(req, res){
     res.write('<body><h1>Hello from my NodeJS Server</h1></body');
     res.write('<html>');
     res.end();
-    
+
 }
 
 const server = http.createServer(rqListener);
