da208685a66ded62cd5bcc38cf08ba8963d90c19
Mark Leo <ratrateroo@gmail.com>
2019-09-06 23:35:12 +0800

    tested some requests

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 //nodejs use require for imports
 
 function rqListener(req, res){
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
 }
 
 const server = http.createServer(rqListener);
