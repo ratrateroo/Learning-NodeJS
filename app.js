43d8b22e070e02f5ee5c312ae4397f9f560c30f9
Mark Leo <ratrateroo@gmail.com>
2019-09-06 23:49:10 +0800

    created a response

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const http = require('http');
 
 function rqListener(req, res){
     console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Welcome Page</title></head>');
+    res.write('<body><h1>Hello from my NodeJS Server</h1></body');
+    res.write('<html>');
+    res.end();
+    
 }
 
 const server = http.createServer(rqListener);
