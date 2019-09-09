802a385819be4de6c0c4038bb92ceca6dbeb8c88
Mark Leo <ratrateroo@gmail.com>
2019-09-10 00:38:07 +0800

    added 404 error page

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ app.use(adminRoutes);
 //default route
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>');
+});
+
 //express created the server
 app.listen(3000);
