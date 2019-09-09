39c9b4e5a198bd0e0a56c67d464dade6e62ad9e6
Mark Leo <ratrateroo@gmail.com>
2019-09-10 00:07:35 +0800

    added body parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //nodejs use require for imports
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //const routes = require('./routes');
 
@@ -17,13 +18,23 @@ const app = express();
     //to the next middleware in line
 }); */
 
+//parse first before routes
+app.use(bodyParser.urlencoded({extended: false}));// added a middleware to parse the request body
+
+//routing
 app.use('/add-product', (req, res, next) => {
-    console.log("In another middleware!");
-    res.send('<h1>Add Product Page</h1>');
+    
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>');
+    
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+//default route
 app.use('/', (req, res, next) => {
-    console.log("In another middleware!");
+    
     res.send('<h1>Hello from express</h1>');
 });
 
