04cee59d8a602b65c5683e264f8b0cf6c9b9a121
Mark Leo <ratrateroo@gmail.com>
2019-09-11 05:31:40 +0800

    changed template to ejs

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const expressHbs = require('express-handlebars');
+// using EJS not handlebars const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({
+/*  app.engine('hbs', expressHbs({
     layoutsDir: 'views/layouts/',
     defaultLayout: 'main-layouts',
     extname: 'hbs'
-}));
+})); */
 
 //app.set('view engine', 'pug');
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
