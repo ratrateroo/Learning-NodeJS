53be36afd5dbc412abe9c08bf9c9438f7998c834
Mark Leo <ratrateroo@gmail.com>
2019-09-13 04:55:39 +0800

    added orders page

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,6 +29,16 @@ exports.getCart = (req, res, next) => {
     });
 };
 
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    });
+};
+
+
+
+
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
