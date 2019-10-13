033adbb6ac6a3768f08a4bd04778688032a69f3a
Mark Leo <ratrateroo@gmail.com>
2019-10-13 20:16:22 +0800

    completed product edit feature

--- models/product.js
+++ models/product.js
@@ -20,7 +20,8 @@ const getProductsFromFile = callback => {
 
 
 module.exports = class Product {    
-    constructor(title, imageUrl, description, price) {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
@@ -28,12 +29,25 @@ module.exports = class Product {
     }
 
     save() {
-        this.id = Math.random().toString();
-        getProductFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), error => {
-                console.log(error);
-            });
+        getProductsFromFile(products => {
+            if (this.id) {
+                const existingProductIndex = products.findIndex(
+                    prod => prod.id === this.id
+                );
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducts), error => {
+                    console.log(error);
+                });
+
+            } else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), error => {
+                    console.log(error);
+                });
+            }
+            
         });
         
         
