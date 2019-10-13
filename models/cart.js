be2cfeb5a6589da845f15a400d6caad23daba55d
Mark Leo <ratrateroo@gmail.com>
2019-10-14 00:44:17 +0800

    displayed cart items in cart page

--- models/cart.js
+++ models/cart.js
@@ -58,4 +58,15 @@ module.exports = class Cart {
         });
     }
 
+    static getCart(callback) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err) {
+                callback(null);
+            } else {
+                callback(cart);
+            }
+        });
+    }
+
 };
