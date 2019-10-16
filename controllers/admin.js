2fc724c10c894ca6b56da2df4f918ec9de663bd1
Mark Leo <ratrateroo@gmail.com>
2019-10-16 21:57:33 +0800

    updated products

--- controllers/admin.js
+++ controllers/admin.js
@@ -39,7 +39,8 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId; //getting the id form the url
-    Product.findById(prodId, product => {
+    Product.findByPk(prodId)
+    .then(product => {
         if (!product) {
             return res.redirect('/');
         }
@@ -50,7 +51,8 @@ exports.getEditProduct = (req, res, next) => {
             editing: editMode,
             product: product
         }); 
-    });
+    })
+    .catch(err => console.log(err));
        
 };
 
@@ -60,15 +62,19 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(
-        prodId, 
-        updatedTitle, 
-        updatedImageUrl, 
-        updatedDesc, 
-        updatedPrice
-        );
-    updatedProduct.save();
-    res.redirect('/admin/products');
+    Product.findByPk(prodId)
+    .then(product => {
+        product.title = updatedTitle;
+        product.price = updatedPrice;
+        product.description = updatedDesc;
+        product.imageUrl = updatedImageUrl;
+        return product.save();
+    })
+    .then(result => {
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
