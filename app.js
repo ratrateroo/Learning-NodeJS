c6e8ad52a548485ebff4bea46e0956db11d647fd
Mark Leo <ratrateroo@gmail.com>
2019-10-16 22:49:38 +0800

    added one-to-many relationship

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 // using EJS not handlebars const expressHbs = require('express-handlebars');
 
@@ -43,7 +45,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result =>{
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize.sync({force: true})
+.then(result =>{
     //console.log(result);
     app.listen(3000);
 })
