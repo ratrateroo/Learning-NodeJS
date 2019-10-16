af3e7573f6476907af1b7406376055d61b62dbf4
Mark Leo <ratrateroo@gmail.com>
2019-10-17 03:01:45 +0800

    added order model

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 // using EJS not handlebars const expressHbs = require('express-handlebars');
 
@@ -64,7 +66,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
-
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem});
 sequelize
 //.sync({force: true}) // for overwriting table
 .sync()
