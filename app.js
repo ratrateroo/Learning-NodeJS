a46895fb00c0bcdfbb88b16851a25c4a656a19d5
Mark Leo <ratrateroo@gmail.com>
2019-10-18 01:06:20 +0800

    installed mongodb driver

--- app.js
+++ app.js
@@ -4,13 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./util/database');
+
 
 // using EJS not handlebars const expressHbs = require('express-handlebars');
 
@@ -26,16 +21,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
-// db.execute('SELECT * FROM products')
-//     .then((result) => {
-//         console.log(result[0]);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
+
 
 //parse first before routes
 app.use(bodyParser.urlencoded({extended: false}));// added a middleware to parse the request body
@@ -43,54 +32,24 @@ app.use(bodyParser.urlencoded({extended: false}));// added a middleware to parse
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-    .then(
-        user => {
-            req.user = user;
-            next();
-        }
-    )
-    .catch(err => console.log(err));
+    // User.findByPk(1)
+    // .then(
+    //     user => {
+    //         req.user = user;
+    //         next();
+    //     }
+    // )
+    // .catch(err => console.log(err));
 });
-//routes
-app.use('/admin', adminRoutes);
+// //routes
+// app.use('/admin', adminRoutes);
 
-//default route
-app.use(shopRoutes);
+// //default route
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem});
-sequelize
-//.sync({force: true}) // for overwriting table
-.sync()
-.then(result =>{
-    return User.findByPk(1);
-    //console.log(result);
-})
-.then(user => {
-    if (!user) {
-        return User.create({ name: 'Mark', email: 'test@test.com'});
-    }
-    return Promise.resolve(user); //promise must return promise for correct chaining
-})
-.then(user => {
-    //console.log(user);
-    return user.createCart();
-})
-.then(cart => {
+mongoConnect((client) => {
+    console.log(client);
     app.listen(3000);
-})
-.catch(err => {
-        console.log(err);
 });
-
-//express created the server
--- util/database.js
+++ util/database.js
@@ -1,8 +1,19 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete2', 'root', 'ultrapassword', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+    MongoClient
+        .connect(
+            'mongodb+srv://mark:ultrapassword@cluster0-oehn6.mongodb.net/test?retryWrites=true&w=majority'
+        )
+        .then(client => {
+            console.log('Connected!');
+            callback(client);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+module.exports = mongoConnect;
