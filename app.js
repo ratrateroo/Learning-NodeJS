a0e244bfd0fd46a9f4ce1b90b5fe9ec312bc63a9
Mark Leo <ratrateroo@gmail.com>
2019-10-18 01:47:00 +0800

    created a product

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 
 // using EJS not handlebars const expressHbs = require('express-handlebars');
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
     //     }
     // )
     // .catch(err => console.log(err));
+    next();
 });
 //routes
 app.use('/admin', adminRoutes);
--- models/product.js
+++ models/product.js
@@ -1,4 +1,4 @@
-const getDB = require('../util/database').getDB();
+const getDB = require('../util/database').getDB;
 
 class Product {
     constructor(title, price, description, imageUrl) {
@@ -9,32 +9,16 @@ class Product {
     }
 
     save() {
-
+        const db = getDB();
+        return db.collection('products')
+        .insertOne(this)
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
     }
 }
-
-const Product = sequelize.define('product',
-{
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    title: Sequelize.STRING,
-    price: {
-        type: Sequelize.DOUBLE,
-        allowNull: false
-    },
-    imageUrl: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-}
-);
 
 module.exports = Product;
