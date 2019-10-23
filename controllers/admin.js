fd8726c64b5b1a0a9699fd8fd78cb22a60865e49
Mark Leo <ratrateroo@gmail.com>
2019-10-23 22:51:44 +0800

    updated a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -69,25 +69,23 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
     
-    const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        prodId
-        );
-    
-    product
-    .save()     
-    .then(result => {
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+            return product.save();
+        })
+        .then(result => {
         console.log('UPDATED PRODUCT!');
         res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
     .then(products => {
             res.render('admin/products', {
                 prods: products,
