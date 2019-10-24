9e905b6332e3a621eec2c50428d8f442e7dfa8ac
Mark Leo <ratrateroo@gmail.com>
2019-10-25 03:10:16 +0800

    stored sessions to mongodb

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session =  require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://mark:ultrapassword@cluster0-oehn6.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,7 +31,12 @@ app.use(bodyParser.urlencoded({extended: false}));// added a middleware to parse
 //serve static files to the file system as read-only
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({ secret: 'my secret', resave: false, saveUninitialized: false })
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
 );
 
 app.use((req, res, next) => {
@@ -47,8 +59,9 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(
-    'mongodb+srv://mark:ultrapassword@cluster0-oehn6.mongodb.net/shop?retryWrites=true&w=majority'
+mongoose
+.connect(
+    MONGODB_URI
     )
 .then(result => {
     User.findOne().then(user => {
