60408634786c6a166bde8d3e1728746fc3a67d45
Mark Leo <ratrateroo@gmail.com>
2019-10-25 00:03:45 +0800

    set up a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,9 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req
+        .get('Cookie')
+        .split(';')[1]
+        .trim()
+        .split('=')[1];
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -7,6 +12,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true');
     res.redirect('/');
 };
