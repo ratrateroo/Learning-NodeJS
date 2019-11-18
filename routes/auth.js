e2d50808dd2e1ab1782aeb5194fba4aea1f4620e
Mark Leo <ratrateroo@gmail.com>
2019-11-18 23:06:46 +0800

    added more validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const {
+    check,
+    body
+} = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +14,30 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post('/signup',
+    [
+        check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, {
+            req
+        }) => {
+            if (value === 'test@test.com') {
+
+                throw new Error('This email address is forbidden.');
+            }
+            return true;
+        }),
+        body(
+            'password',
+            'Please enter a password with only numbers and text and at least 5 characters.'
+        )
+        .isLength({
+            min: 5
+        })
+        .isAlphanumeric()
+    ],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
