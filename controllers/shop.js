13c2f85499f9ed741db51b46913d78d48f5214b2
Mark Leo <ratrateroo@gmail.com>
2019-11-22 22:48:02 +0800

    added dynamic pagination buttons

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,7 +6,7 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -43,10 +43,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1;
   let totalItems;
   Product.find()
-    .count()
+    .countDocuments()
     .then(numProducts => {
       totalItems = numProducts;
       return Product.find()
@@ -58,7 +58,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
