819674151c39cb6ed0e90932570a222f2d6a0688
Mark Leo <ratrateroo@gmail.com>
2019-11-23 00:19:13 +0800

    added dom manipulation for deleted product async

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const fileHelper = require('../util/file');
+
 const { validationResult } = require('express-validator/check');
 
 const Product = require('../models/product');
@@ -199,25 +201,25 @@ exports.getProducts = (req, res, next) => {
         });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
       .then(product => {
         if (!product) {
           return next(new Error('Product not found.'));
         }
         if(product.imageUrl) {
-          fileHelper.deleteFile(product.imageUrl);
+            fileHelper.deleteFile(product.imageUrl);
+            return Product.deleteOne({ _id: prodId, userId: req.user._id });
         }
-        return Product.deleteOne({ _id: prodId, userId: req.user._id });
+        
       })
       .then(() => {
         console.log('DESTROYED PRODUCT');
-        res.redirect('/admin/products');
+        res.status(200).json({message: 'Success!'});
       })
       .catch(err => {
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
+        console.log(err);
+        res.status(500).json({message: 'Deleting product failed.'});
       });
   };
--- public/js/admin.js
+++ public/js/admin.js
@@ -2,5 +2,23 @@ const deleteProduct = (btn) => {
     console.log('Clicked Delete Button');
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
-
+    
+    const productElement = btn.closest('article');
+    
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    })
+    .then(result => {
+        return result.json();
+    })
+    .then(data => {
+        console.log(data);
+        productElement.parentNode.removeChild(productElement);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 };
